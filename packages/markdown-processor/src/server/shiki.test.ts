2fc0661d5c3b0498cca150108117f0ce0621b294
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-11-14 00:58:26 +0900

    style: format 2

--- packages/markdown-processor/src/server/shiki.test.ts
+++ packages/markdown-processor/src/server/shiki.test.ts
@@ -1,23 +1,29 @@
-import { vi } from "vitest"
+import { vi } from "vitest";
 
-import { getShikiHighlighter } from "./shiki.js"
+import { getShikiHighlighter } from "./shiki.js";
 
 vi.mock("shiki", async (importActual) => {
-  const actual = await importActual<typeof import("shiki")>()
+  const actual = await importActual<typeof import("shiki")>();
   return {
     ...actual,
     // lang, theme はこのテストでは使わないし時間がかかるので省略
-    createHighlighterCore: vi.fn().mockImplementation(() => actual.createHighlighterCore({ engine: actual.createJavaScriptRegexEngine() })),
-  }
-})
+    createHighlighterCore: vi
+      .fn()
+      .mockImplementation(() =>
+        actual.createHighlighterCore({
+          engine: actual.createJavaScriptRegexEngine(),
+        }),
+      ),
+  };
+});
 
 describe("getShikiHighlighter", () => {
   it("should memoize highlighter instance", async () => {
-    const h1 = await getShikiHighlighter()
-    const h2 = await getShikiHighlighter()
-    expect(h1).toBe(h2)
+    const h1 = await getShikiHighlighter();
+    const h2 = await getShikiHighlighter();
+    expect(h1).toBe(h2);
 
-    const { createHighlighterCore } = await import("shiki")
-    expect(createHighlighterCore).toHaveBeenCalledTimes(1)
-  })
+    const { createHighlighterCore } = await import("shiki");
+    expect(createHighlighterCore).toHaveBeenCalledTimes(1);
+  });
 });
