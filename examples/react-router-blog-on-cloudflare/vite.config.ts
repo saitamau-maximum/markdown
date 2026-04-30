0229bbf8b7bbeb9c6e7c75229a80f28defdb74e2
Asa <26807394+a01sa01to@users.noreply.github.com>
2026-05-01 00:45:39 +0900

    chore: upgrade react-router, wrangler, vite

--- examples/react-router-blog-on-cloudflare/vite.config.ts
+++ examples/react-router-blog-on-cloudflare/vite.config.ts
@@ -1,12 +1,13 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true
+  },
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     reactRouter(),
-    tsconfigPaths(),
   ],
 });
